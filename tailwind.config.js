7672f6ef0ae33138f69f6484af56b5f913e35efb
Jorge Robles <robles8925@gmail.com>
2024-02-16 17:03:02 -0500

    merged with flask

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./static/**/*.js", "./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
